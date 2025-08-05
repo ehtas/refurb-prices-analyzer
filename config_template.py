c2c874e4bdb583ad4613e48e019ffd5778a42182
Md Ehtasham Ahmed <59911140+ehtas@users.noreply.github.com>
2025-08-05 13:16:26 +0530

    Update config_template.py

--- config_template.py
+++ config_template.py
@@ -1,26 +1,38 @@
-# config_template.py
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+# Database configuration
+DATABASE_URL = "sqlite:///create_a_db_file_in_root"
+
+# Scraper configurations
 SCRAPER_CONFIG = {
     "cashify": {
-        "base_url": "https://your-url-here.com"
+        "base_url": "https://www.cashify.in"
     },
-    "maple": {
-        "base_url": "https://your-url-here.com"
-    },
-    "refitglobal": {
-        "base_url": "https://your-url-here.com"
+    "refitGlobal":{
+        "base_url": "https://refitglobal.com/"
     },
     "mobilegoo": {
-        "base_url": "https://your-url-here.com"
-    },
-    "amazon": {
-        "base_url": "https://your-url-here.com"
+        "base_url": "https://mobilegoo.shop"
     },
     "flipkart": {
-        "base_url": "https://your-url-here.com"
+        "base_url": "https://www.flipkart.com/search?q=refurbished+mobiles"
     },
     "quikr": {
-        "base_url": "https://your-url-here.com"
+        "base_url": "https://www.quikr.com/mobiles-tablets/Mobile-Phones+India+y149f"
+    },
+    "maple": {
+        "base_url": "https://www.maplestore.in"
     }
 }
 
+
+
+# User authentication (simple demo)
+USERS = {
+    "admin": "admin123",
+    "user": "user123"
+}
+AMAZON_API_KEY = ""
